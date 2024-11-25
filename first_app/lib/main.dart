1ae0316c1aeebed2b375bc4fd12ccf7418d17e71
Anupam Gupta <64581827+anupam-dosar@users.noreply.github.com>
2024-11-25 15:58:33 +0530

    Update main.dart

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,37 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+  runApp(
+    const MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('First App'),
-        ),
+        backgroundColor: Colors.deepPurple,
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text(
-                'Hello World!',
-              ),
-              Text(
-                'It\'s time to learn Flutter!',
-              ),
-            ],
-          ),
+          child: Text('Hello Anupam!'),
         ),
       ),
-    );
-  }
+    ),
+  );
 }
